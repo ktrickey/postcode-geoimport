55a9dbe3de276f5aa91380714eb5d0d5fe03c3b3
Kevin Trickey <registrations@kevintrickey.me.uk>
2018-08-07 21:45:50 +0100

    Initial Commit
    COmmand line import to local db, no config possible

---
+++ src/Import/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Import
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var importer = new PostcodeImporter.Importer();
+            await importer.ImportPostcodes(new FileStream(@"c:\mongo\ukpostcodes.csv", FileMode.Open, FileAccess.Read));
+            
+            Console.WriteLine("Complete");
+        }
+    }
+}
---
+++ src/PostcodeImporter/PostcodeImporter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper.TypeConversion;
+using MongoDB;
+using MongoDB.Driver;
+
+namespace PostcodeImporter
+{
+    
+    internal class PostcodeData
+    {
+        
+        public long Id { get; set; }
+        public string Postcode { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+    
+    public class Importer
+    {
+        public async Task ImportPostcodes(Stream csvStream)
+        {
+            using (var streamReader = new StreamReader(csvStream))
+            {
+                using (var csvReader = new CsvHelper.CsvReader(streamReader))
+                {
+                   
+                    var client = new MongoClient("mongodb://localhost:32773");
+                    var database = client.GetDatabase("HousePrice");
+                    database.DropCollection("Postcodes");
+                    var collection = database.GetCollection<PostcodeData>("Postcodes");
+			
+                    var batch = new List<PostcodeData>();
+                    long lastId = 0;
+                    
+                    while( csvReader.Read() )
+                    {
+                        try
+                        {
+                            var record = csvReader.GetRecord<PostcodeData>();
+                            record.Postcode = record.Postcode.Replace(" ", String.Empty);
+                            lastId = record.Id;
+                            batch.Add(record);
+
+                        }
+
+                        catch (TypeConverterException)
+                        {
+                            // invalid lat/long so ignore
+                        }
+                        
+                        if (batch.Count == 1000)
+                        {
+                            await collection.InsertManyAsync(batch);
+                            batch.Clear();
+                        }
+                            
+                    }
+
+                    if (batch.Any())
+                    {
+                        await collection.InsertManyAsync(batch);
+                    }
+
+                    Console.WriteLine("Generating index...");
+                    var notificationLogBuilder = Builders<PostcodeData>.IndexKeys;
+                    var indexModel = new CreateIndexModel<PostcodeData>(notificationLogBuilder.Ascending(x => x.Postcode));
+                    await collection.Indexes.CreateOneAsync(indexModel).ConfigureAwait(false);
+                }
+            }
+        }
+    }
+}
